64c07f14315dcc75e04fb26bbe5852859da48050
wilfried <wilfried.odorico@gmail.com>
2022-09-08 16:33:26 +0200

    script import corriger et ajout des exceptions

--- import.py
+++ import.py
@@ -2,12 +2,11 @@ import requests
 import json
 import unicodedata
 
+
 open_quizz_db_data = (
-    ("Animaux", "Les chats", "https://www.kiwime.com/oqdb/files/1050828847/OpenQuizzDB_050/openquizzdb_50.json"),
-    ("Arts", "Musée du Louvre", "https://www.kiwime.com/oqdb/files/1086624389/OpenQuizzDB_086/openquizzdb_86.json"),
-    ("Bande dessinnée", "Tintin", "https://www.kiwime.com/oqdb/files/2124627384/OpenQuizzDB_124/openquizzdb_124.json"),
-    ("Cinéma", "Alien", "https://www.kiwime.com/oqdb/files/3241454997/OpenQuizzDB_241/openquizzdb_241.json"),
-    ("Cinéma", "Star wars", "https://www.kiwime.com/oqdb/files/1090993427/OpenQuizzDB_090/openquizzdb_90.json"),
+    ("Animaux", "Animaux en vrac", "https://www.kiwime.com/oqdb/files/2184923922/OpenQuizzDB_184/openquizzdb_184.json"),
+    ("Culture générale", "Culture en vrac", "https://www.kiwime.com/oqdb/files/2187579277/OpenQuizzDB_187/openquizzdb_187.json"),
+    ("Comic", "Tintin", "https://www.kiwime.com/oqdb/files/2124242395/OpenQuizzDB_124/openquizzdb_124.json"),
 )
 
 
@@ -22,27 +21,35 @@ def get_quizz_filename(categorie, titre, difficulte):
 def generate_json_file(categorie, titre, url):
     out_questionnaire_data = {"categorie": categorie, "titre": titre, "questions": []}
     out_questions_data = []
-    response = requests.get(url)
-    data = json.loads(response.text)
-    all_quizz = data["quizz"]["fr"]
-    for quizz_title, quizz_data in all_quizz.items():
-        out_filename = get_quizz_filename(categorie, titre, quizz_title)
-        print(out_filename)
-        out_questionnaire_data["difficulte"] = quizz_title
-        for question in quizz_data:
-            question_dict = {}
-            question_dict["titre"] = question["question"]
-            question_dict["choix"] = []
-            for ch in question["propositions"]:
-                question_dict["choix"].append((ch, ch==question["réponse"]))
-            out_questions_data.append(question_dict)
-        out_questionnaire_data["questions"] = out_questions_data
-        out_json = json.dumps(out_questionnaire_data)
+    try:
+        response = requests.get(url)
+    except:
+        print(f"Exeception pour l'url: {url}")
+    else:
+        try:
+            data = json.loads(response.text)
+            all_quizz = data["quizz"]["fr"]
+            for quizz_title, quizz_data in all_quizz.items():
+                out_filename = get_quizz_filename(categorie, titre, quizz_title)
+                print(out_filename)
+                out_questionnaire_data["difficulte"] = quizz_title
+                for question in quizz_data:
+                    question_dict = {}
+                    question_dict["titre"] = question["question"]
+                    question_dict["choix"] = []
+                    for ch in question["propositions"]:
+                        question_dict["choix"].append((ch, ch==question["réponse"]))
+                    out_questions_data.append(question_dict)
+                out_questionnaire_data["questions"] = out_questions_data
+                out_json = json.dumps(out_questionnaire_data)
 
-        file = open(out_filename, "w")
-        file.write(out_json)
-        file.close()
-        print("end")
+                file = open(out_filename, "w")
+                file.write(out_json)
+                file.close()
+                print("end")
+        except:
+            print(f"Exeception data pour {titre} avec l'url: {url}")
+    
 
 
 for quizz_data in open_quizz_db_data:
